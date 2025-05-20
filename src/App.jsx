3ab52b709e70ac9cfb6542b3808f0f8783e7aebd
Noah Elias <noahelias555@gmail.com>
2025-05-20 16:37:32 +0100

    added sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -5,17 +6,26 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
+      <main>
+        <section id="hero">
+          <Hero />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
